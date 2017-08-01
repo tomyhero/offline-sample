cc398d2f0216085fb1bba5d231f6e255afac3780
Your Name <you@example.com>
2017-08-01 16:44:40 +0900

    first commit

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+
+  event.waitUntil(
+      caches.open('v1').then(function(cache) {
+              return cache.addAll([ './style.css' ]);
+      })
+  );
+});
