6028efd19d173cec143d1c4db0a0d72f9eab7355
Your Name <you@example.com>
2017-08-01 17:59:42 +0900

    .

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
       caches.open('v1').then(function(cache) {
-              return cache.addAll([ './style.css','./icon.png' ]);
+              return cache.addAll([ './style.css','./icon.png','https://ad-center-line-me.s3-ap-northeast-1.amazonaws.com/files/topics/943_ext_01_0.pdf?response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27mediaguide_LINE%2520Audience%2520Match_2017_7-9.pdf&response-content-type=application%2Fpdf&AWSAccessKeyId=AKIAI2J4E36RBCZRMWIA&Expires=1501579672&Signature=SNTtDAMPOHToydL25QS8KC6bIEs%3D&.pdf']);
       })
   );
 });
